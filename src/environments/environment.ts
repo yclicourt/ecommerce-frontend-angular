ec6cfbbd07cb5ae5635053122650659193920682
Yoadev <yclicourt@gmail.com>
2025-06-22 17:26:52 -0400

    update: Clean environments to production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ecommerce-backend-4tr1.onrender.com/api/v1',
-  clientUrl:'http://localhost:4200/',
-  brandName:'Ecommerce Yoyo Shop',
-  tokenKey: 'tokenKey',
-  currentUser: 'currentUser',
-  PAYPAL_API: 'https://api-m.sandbox.paypal.com',
+  apiUrl: '',
+  clientUrl:'',
+  brandName:'',
+  tokenKey: '',
+  currentUser: '',
+  PAYPAL_API: '',
 };
